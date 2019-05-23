2fc83287e020ec96b3ef115837a28d412160ab40
Wellington Viveiro <wellyviveiro@gmail.com>
2019-05-24 09:31:08 +1000

    Add capacity for Hour minutes and seconds. Create function to verify if date is valid and function to get the difference between two dates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * strdate format 'YYYY-MM-DD'
+ * strdate format 'YYYY-MM-DD HH:mm:ss'
  * If empty strdate, get current date
  *
  * @author Wellington Viveiro <wviveiro@gmail.com>
@@ -7,14 +7,33 @@
 const date = (strdate) => {
     // Format number function to add lead 0
     const f = (num) => `0${num}`.substr(`0${num}`.length - 2);
+    
 
     if (!strdate) {
         const currdt = new Date();
-        strdate = `${currdt.getFullYear()}-${f(currdt.getMonth() + 1)}-${f(currdt.getDate())}`;
+        strdate = `${currdt.getFullYear()}-${f(currdt.getMonth() + 1)}-${f(currdt.getDate())} ${f(currdt.getHours())}:${f(currdt.getMinutes())}:${f(currdt.getSeconds())}`;
     }
 
+    let hourSplit = strdate.split(' ');
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    if (hourSplit.length > 1) {
+        let hrs = hourSplit[1].split(':');
+        if (hrs.length === 3) {
+            hours = hrs[0];
+            minutes = hrs[1];
+            seconds = hrs[2];
+        }
+        strdate = hourSplit[0];
+    }
+    
     const aux = strdate.split('-');
-    const dtObj = new Date(aux[0], +aux[1] - 1, aux[2]);
+    const dtObj = new Date(aux[0], +aux[1] - 1, aux[2], hours, minutes, seconds);
+
+    const isValid = () => {
+        return dtObj instanceof Date && !isNaN(dtObj);
+    }
 
     const getDateObj = () => {
         return dtObj;
@@ -52,10 +71,48 @@ const date = (strdate) => {
         return auxformat;
     }
 
+    const diff = (date_compare) => {
+        const dt = date(date_compare);
+        if (!dt.isValid()) return false;
+
+        let milliseconds = dt.getDateObj() - dtObj;
+        if (milliseconds < 0) milliseconds *= -1;
+
+        let calcYear = (1000 * 60 * 60 * 24 * 365);
+        let years = Math.floor(milliseconds / calcYear);
+        milliseconds -= years * calcYear;
+
+        let calcMonth = (1000 * 60 * 60 * 24 * 30.417);
+        let months = Math.floor(milliseconds / calcMonth);
+        milliseconds -= months * calcMonth;
+
+        let calcDays = (1000 * 60 * 60 * 24);
+        let days = Math.floor(milliseconds / calcDays);
+        milliseconds -= days * calcDays;
+
+        let calcHours = (1000 * 60 * 60);
+        let hours = Math.floor(milliseconds / calcHours);
+        milliseconds -= hours * calcHours;
+
+        let calcMinutes = (1000 * 60);
+        let minutes = Math.floor(milliseconds / calcMinutes);
+        milliseconds -= minutes * calcMinutes;
+
+        let calcSeconds = (1000);
+        let seconds = Math.floor(milliseconds / calcSeconds);
+        milliseconds -= seconds * calcSeconds;
+
+        
+        return {years, months, days, hours, minutes, seconds};
+
+    }
+
     return {
         getDateObj,
         getFullDate,
         modify,
+        isValid,
+        diff,
         format
     }
 }
