92a4bd1bcc7dbeb57f3ccef603e98823027d8f14
Wellington Viveiro <wellyviveiro@gmail.com>
2019-05-24 20:40:12 +1000

    make hour minute and second default 00:00:00

--- index.js
+++ index.js
@@ -1,35 +1,73 @@
 /**
- * strdate format 'YYYY-MM-DD HH:mm:ss'
+ * strdate
  * If empty strdate, get current date
  *
  * @author Wellington Viveiro <wviveiro@gmail.com>
  **/
-const date = (strdate) => {
+const date = (strdate, _format) => {
     // Format number function to add lead 0
-    const f = (num) => `0${num}`.substr(`0${num}`.length - 2);
+    const f = (num, lead = 2) => {
+        let str = '';
+        for (let i = 1; i <= lead; i++) str += '0';
+
+        return `${str}${num}`.substr(`${str}${num}`.length - lead);
+    }
     
 
     if (!strdate) {
         const currdt = new Date();
         strdate = `${currdt.getFullYear()}-${f(currdt.getMonth() + 1)}-${f(currdt.getDate())} ${f(currdt.getHours())}:${f(currdt.getMinutes())}:${f(currdt.getSeconds())}`;
+        _format = 'YYYY-MM-DD HH:mm:ss';
     }
 
-    let hourSplit = strdate.split(' ');
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    if (hourSplit.length > 1) {
-        let hrs = hourSplit[1].split(':');
-        if (hrs.length === 3) {
-            hours = hrs[0];
-            minutes = hrs[1];
-            seconds = hrs[2];
-        }
-        strdate = hourSplit[0];
+    if (!_format) {
+        _format = 'YYYY-MM-DD';
     }
-    
-    const aux = strdate.split('-');
-    const dtObj = new Date(aux[0], +aux[1] - 1, aux[2], hours, minutes, seconds);
+
+    const buildDate = () => {
+        let value = _format.replace(/[YMDHms]/gi, '');
+
+        //remove repeat chars
+        value = value.replace(/(.)(?=.*\1)/gi, '');
+        
+        value = new RegExp(`[${value}]`, 'gi');
+        let fm = _format.replace(value, '-').split('-');
+        let dt = strdate.replace(value, '-').split('-');
+        if (fm.length !== dt.length) return false;
+
+        let year, month, day, hour, minute, second;
+
+        fm.forEach((fmt, index) => {
+            if (fmt.indexOf('Y') > -1) {
+                year = dt[index];
+            } else if (fmt.indexOf('M') > -1) {
+                month = f(dt[index]);
+            } else if (fmt.indexOf('D') > -1) {
+                day = f(dt[index]);
+            } else if (fmt.indexOf('H') > -1) {
+                hour = f(dt[index]);
+            } else if (fmt.indexOf('m') > -1) {
+                minute = f(dt[index]);
+            } else if (fmt.indexOf('s') > -1) {
+                second = f(dt[index]);
+            }
+        });
+
+        if (!year || !month || !day) return false;
+
+        let str = `${year}-${month}-${day}`;
+        if (!hour) hour = 0;
+        if (!minute) minute = 0;
+        if (!second) second = 0;
+        str += `T${f(hour)}:${f(minute)}:${f(second)}`;
+
+
+        return new Date(str);
+
+        
+    }
+
+    const dtObj = buildDate();
 
     const isValid = () => {
         return dtObj instanceof Date && !isNaN(dtObj);
@@ -41,6 +79,8 @@ const date = (strdate) => {
     
 
     const getFullDate = () => {
+        if (!isValid()) return false;
+
         const newStrDate = `${dtObj.getFullYear()}-${f(dtObj.getMonth() + 1)}-${f(dtObj.getDate())}`;
         return newStrDate;
     }
@@ -48,24 +88,55 @@ const date = (strdate) => {
     const modify = (num, type) => {
         if (type.substr(0, 3) === 'day') {
             dtObj.setDate(dtObj.getDate() + num);
-        }else if (type.substr(0, 5) === 'month') {
+        } else if (type.substr(0, 5) === 'month') {
             dtObj.setMonth(dtObj.getMonth() + num);
+        } else if (type.substr(0, 5) === 'year') {
+            dtObj.setFullYear(dtObj.getFullYear() + num);
         }
     }
 
     const format = (strformat) => {
+        if (!isValid()) return false;
+
         let auxformat = strformat;
-        auxformat = auxformat.replace(/YYYY/gi, dtObj.getFullYear());
-        auxformat = auxformat.replace(/MM/gi, f(dtObj.getMonth() + 1));
-        auxformat = auxformat.replace(/M/gi, dtObj.getMonth() + 1);
-        auxformat = auxformat.replace(/DD/gi, f(dtObj.getDate()));
-        auxformat = auxformat.replace(/D/gi, dtObj.getDate());
 
+        // Year Format
+        let year = `${dtObj.getFullYear()}`;
+        auxformat = auxformat.replace(/YYYY/gi, year);
+        auxformat = auxformat.replace(/YY/gi, year.substr(year.length - 2));
+
+        // Month format
+        let month = `${dtObj.getMonth() + 1}`;
+        auxformat = auxformat.replace(/MM/gi, f(month));
+        auxformat = auxformat.replace(/M/gi, month);
+
+
+        // Day Format
+        let day = `${dtObj.getDate()}`;
+        auxformat = auxformat.replace(/DDD/gi, f(day, 3));
+        auxformat = auxformat.replace(/DD/gi, f(day));
+        auxformat = auxformat.replace(/D/gi, day);
+
+        // Hour format
+        let hours = +dtObj.getHours();
+        auxformat = auxformat.replace(/HH/gi, f(hours));
+        auxformat = auxformat.replace(/H/gi, hours);
+        let not24 = (hours > 12) ? hours - 12 : (hours === 0) ? 12 : hours;
+        auxformat = auxformat.replace(/hh/gi, f(not24));
+        auxformat = auxformat.replace(/h/gi, not24);
+        auxformat = auxformat.replace(/a/gi, (hours > 12) ? 'pm' : 'am');
+
+        let minutes = +dtObj.getMinutes();
+        auxformat = auxformat.replace(/mm/gi, f(minutes));
+        auxformat = auxformat.replace(/m/gi, minutes);
+
+        let seconds = +dtObj.getSeconds();
+        auxformat = auxformat.replace(/ss/gi, f(seconds));
+        auxformat = auxformat.replace(/s/gi, seconds);
+
+        // Week day format
         let week = dtObj.getDay();
-        
         if (+week === 0) week = 7;
-
-
         auxformat = auxformat.replace(/N/gi, week);
 
         return auxformat;
