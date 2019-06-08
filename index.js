a276eef98942ba250dcf77f4215f61f0a140b4ce
Wellington Viveiro <wellyviveiro@gmail.com>
2019-06-08 22:19:46 +1000

    change approach of format date to avoid overlapping

--- index.js
+++ index.js
@@ -104,48 +104,70 @@ const date = (strdate, _format) => {
         if (!isValid()) return false;
 
         let auxformat = strformat;
+        auxformat = auxformat.replace(/YYYY/g, '{0001}'); // Full year
+        auxformat = auxformat.replace(/YY/g, '{0002}'); // Half Yerar
+        auxformat = auxformat.replace(/DDD/g, '{0003}'); // 3 digits day
+        auxformat = auxformat.replace(/DD/g, '{0004}'); // 2 digits day
+        auxformat = auxformat.replace(/D/g, '{0005}'); // 1 digit day
+        auxformat = auxformat.replace(/HH/g, '{0006}'); // Hour 2 digits 24 hours
+        auxformat = auxformat.replace(/H/g, '{0007}'); // Hour 1 digit 24 hours
+        auxformat = auxformat.replace(/hh/g, '{0008}'); // Hour 2 digits 12 hours
+        auxformat = auxformat.replace(/h/g, '{0009}'); // Hour 1 digit 12 hours
+        auxformat = auxformat.replace(/a/g, '{0010}'); // am pm
+        auxformat = auxformat.replace(/mm/g, '{0011}'); // minutes 2 digits
+        auxformat = auxformat.replace(/m/g, '{0012}'); // minutes 1 digit
+        auxformat = auxformat.replace(/ss/g, '{0013}'); // Seconds 2 digits
+        auxformat = auxformat.replace(/s/g, '{0014}'); // Second 1 digit
+        auxformat = auxformat.replace(/N/gi, '{0015}'); // Day of the week
+        auxformat = auxformat.replace(/MMMM/g, '{0016}'); // Month name full
+        auxformat = auxformat.replace(/MMM/g, '{0017}'); // month name 3 letters
+        auxformat = auxformat.replace(/MM/g, '{0018}'); // Month 1 to 12
+        auxformat = auxformat.replace(/M/g, '{0019}'); // Month 1 to 12 1 digit
+
 
         // Year Format
         let year = `${dtObj.getUTCFullYear()}`;
-        auxformat = auxformat.replace(/YYYY/g, year);
-        auxformat = auxformat.replace(/YY/g, year.substr(year.length - 2));
+
+        
+        auxformat = auxformat.replace(/\{0001}/g, year);
+        auxformat = auxformat.replace(/\{0002}/g, year.substr(year.length - 2));
 
 
 
         // Day Format
         let day = `${dtObj.getUTCDate()}`;
-        auxformat = auxformat.replace(/DDD/g, f(day, 3));
-        auxformat = auxformat.replace(/DD/g, f(day));
-        auxformat = auxformat.replace(/D/g, day);
+        auxformat = auxformat.replace(/\{0003}/g, f(day, 3));
+        auxformat = auxformat.replace(/\{0004}/g, f(day));
+        auxformat = auxformat.replace(/\{0005}/g, day);
 
         // Hour format
         let hours = +dtObj.getUTCHours();
-        auxformat = auxformat.replace(/HH/g, f(hours));
-        auxformat = auxformat.replace(/H/g, hours);
+        auxformat = auxformat.replace(/\{0006}/g, f(hours));
+        auxformat = auxformat.replace(/\{0007}/g, hours);
         let not24 = (hours > 12) ? hours - 12 : (hours === 0) ? 12 : hours;
-        auxformat = auxformat.replace(/hh/g, f(not24));
-        auxformat = auxformat.replace(/h/g, not24);
-        auxformat = auxformat.replace(/a/g, (hours > 12) ? 'pm' : 'am');
+        auxformat = auxformat.replace(/\{0008}/g, f(not24));
+        auxformat = auxformat.replace(/\{0009}/g, not24);
+        auxformat = auxformat.replace(/\{0010}/g, (hours > 12) ? 'pm' : 'am');
 
         let minutes = +dtObj.getUTCMinutes();
-        auxformat = auxformat.replace(/mm/g, f(minutes));
-        auxformat = auxformat.replace(/m/g, minutes);
+        auxformat = auxformat.replace(/\{0011}/g, f(minutes));
+        auxformat = auxformat.replace(/\{0012}/g, minutes);
 
         let seconds = +dtObj.getUTCSeconds();
-        auxformat = auxformat.replace(/ss/g, f(seconds));
-        auxformat = auxformat.replace(/s/g, seconds);
+        auxformat = auxformat.replace(/\{0013}/g, f(seconds));
+        auxformat = auxformat.replace(/\{0014}/g, seconds);
 
         // Week day format
         let week = dtObj.getUTCDay();
         if (+week === 0) week = 7;
-        auxformat = auxformat.replace(/N/gi, week);
+        auxformat = auxformat.replace(/\{0015}/gi, week);
         
         // Month format
         let month = `${dtObj.getUTCMonth() + 1}`;
-        auxformat = auxformat.replace(/MMMM/g, Months[month]);
-        auxformat = auxformat.replace(/MMM/g, Months[month].substr(0, 3));
-        auxformat = auxformat.replace(/MM/g, f(month));
-        auxformat = auxformat.replace(/M/g, month);
+        auxformat = auxformat.replace(/\{0016}/g, Months[month]);
+        auxformat = auxformat.replace(/\{0017}/g, Months[month].substr(0, 3));
+        auxformat = auxformat.replace(/\{0018}/g, f(month));
+        auxformat = auxformat.replace(/\{0019}/g, month);
 
         
 
